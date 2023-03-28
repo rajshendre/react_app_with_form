ad7d82672f26ef1a5a7c2476a59f6d49a039a930
Rajat <rajat@nddeo.com>
2023-03-28 10:32:44 +0530

    first commit with having multiple components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import './About.css';
+import './Home.css';
+import './UserForm.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to = '/'>This page </Link></li>
+            <li><Link to = '/home'>Home page </Link></li>
+            <li><Link to = '/about'>About page </Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/Home" element={<Home/>}/>
+          <Route path="/About" element={<About/>}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
